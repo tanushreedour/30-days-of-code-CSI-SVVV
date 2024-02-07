6ba271843946278a6ec5d3c0ebaad4f0cd85c554
tanushreedour <147717390+tanushreedour@users.noreply.github.com>
2024-02-07 18:20:00 +0530

    Add files via upload

---
+++ Day6.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Day6 {
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 2, 56};
+        System.out.println(Arrays.toString(arr));
+
+        int[][] num = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.deepToString(num));
+    }
+}
